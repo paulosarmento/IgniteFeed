24a62f11225d278016c12f1f494dabafac7e0b31
Paulo Sarmento <paulo.cesar.sarmento@hotmail.com>
2022-12-19 18:35:23 -0300

    Componente:Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import styles from "./App.module.css";
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
       <Header />
-      <Post author="Paulo Sarmento" content="lorem ipsum dolor sit amet" />
-      <Post author="Paulo Júnior" content="Um post bem legal" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Paulo Sarmento" content="lorem ipsum dolor sit amet" />
+          <Post author="Paulo Júnior" content="Um post bem legal" />
+        </main>
+      </div>
     </>
   );
 }
