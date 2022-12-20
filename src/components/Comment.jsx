dbcb62e03f1ad36a081b86b3505facf1bf8e2573
Paulo Sarmento <paulo.cesar.sarmento@hotmail.com>
2022-12-20 10:01:09 -0300

    Componente:Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/paulosarmento.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTimeline}>
+              <strong>Paulo Sarmento</strong>
+              <time
+                title="20 de dezembro às 9:52h"
+                dateTime="2022-12-20 09:53:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏</p>
+        </div>
+      </div>
+      <footer>
+        <button>
+          <ThumbsUp />
+          Aplaudir <span>20</span>
+        </button>
+      </footer>
+    </div>
+  );
+}
